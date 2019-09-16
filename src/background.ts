c89e7c273b67a1bc28c4a1e70416485662e22b8b
Peter Repukat <orly.alia5@gmail.com>
2019-09-16 14:55:36 +0200

    add missing file

---
+++ src/background.ts
@@ -0,0 +1,35 @@
+// chrome.runtime.onMessage.addListener(
+//     (request, sender, sendResponse) => {
+//       if (request.contentScriptQuery === "queryPrice") {
+//         let url = "https://another-site.com/price-query?itemId=" +
+//                 encodeURIComponent(request.itemId);
+//         fetch(url)
+//             .then(response => response.text())
+//             .then(text => parsePrice(text))
+//             .then(price => sendResponse(price))
+//             .catch(error => ...)
+//         return true;  // Will respond asynchronously.
+//       }
+//     });
+
+const htmlDecode = (input: string) => {
+    const e = document.createElement('textarea');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+  };
+
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    if (request.contentScriptQuery === 'queryLyrics') {
+        fetch('https://genius.com' + request.pathSuffix).then(async (lyricsResponse) => {
+            try {
+                const testText = await lyricsResponse.text();
+                const testContentText = htmlDecode(testText.match(/<\/script>(\s*)?(\s*)<meta content=\"(.*)\"/)[3]);
+                const testLyrics = testContentText.match(/html\":\"<p>(.*?)<\/p>/)[1].replace(/\\n/g, '').replace(/<a(.*?)>/g, '').replace(/<\/a>/g, '');
+                sendResponse(testLyrics);
+            } catch (e) {
+                sendResponse('Error ... :( \n' + e);
+            }
+        });
+    }
+    return true;
+});
